c66eeaac7eae1e78defbd387dac66f9cb425aede
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-24 02:12:42 +0530

    write junit

--- src/main/java/UserRegistration_20/UserRegistration.java
+++ src/main/java/UserRegistration_20/UserRegistration.java
@@ -33,18 +33,18 @@ public class UserRegistration {
 
 
     public static void main(String[] args) {
-        boolean result = Pattern.matches("[A-Za-z]{3,10}", "GOOD");
-        boolean LastName = Pattern.matches("[A-Za-z]{3,10}", "Gulhane");
-        System.out.println("Given input is RIGHT ? \n"+result);
-        System.out.println("LastName is Valid ? \n"+LastName);
-
-        boolean Email = Pattern.matches("[a-zA-Z0-9]+[@][a-z]+[.][a-z]{2,}", "arpit99@microsoft.in");
-        System.out.println("Email is Valid ? \n"+Email);
-
-        boolean Number = Pattern.matches("[91 ]+[0-9]{10}", "91 9284530926");
-        System.out.println("Number is Valid ? \n"+Number);
-
-        boolean Password = Pattern.matches("(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}", "A2345678%");
-        System.out.println("Password is Valid ? \n"+Password);
+//        boolean result = Pattern.matches("[A-Za-z]{3,10}", "GOOD");
+//        boolean LastName = Pattern.matches("[A-Za-z]{3,10}", "Gulhane");
+//        System.out.println("Given input is RIGHT ? \n"+result);
+//        System.out.println("LastName is Valid ? \n"+LastName);
+//
+//        boolean Email = Pattern.matches("[a-zA-Z0-9]+[@][a-z]+[.][a-z]{2,}", "arpit99@microsoft.in");
+//        System.out.println("Email is Valid ? \n"+Email);
+//
+//        boolean Number = Pattern.matches("[91 ]+[0-9]{10}", "91 9284530926");
+//        System.out.println("Number is Valid ? \n"+Number);
+//
+//        boolean Password = Pattern.matches("(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}", "A2345678%");
+//        System.out.println("Password is Valid ? \n"+Password);
     }
 }
